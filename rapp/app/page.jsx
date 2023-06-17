5893c4c337f420b3aa9083cf85c7369c8d835c55
Spiro <spiroheqimi90@gmail.com>
2023-06-17 21:20:37 +0200

    5th commit

--- rapp/app/page.jsx
+++ rapp/app/page.jsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import spiro from './public/SpiroHeqimi.png'
+/* import Image from 'next/image'
+import spiro from './public/SpiroHeqimi.png' */
 
 export default function Home() {
   return (
