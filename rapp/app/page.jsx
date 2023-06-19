58196baad118cdf702cf65f099f205c03e3c7801
Spiro <spiroheqimi90@gmail.com>
2023-06-19 17:08:20 +0200

    daily commit

--- rapp/app/page.jsx
+++ rapp/app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <main className="top-0 w-screen h-screen overflow-hidden bg-background-color">
-        <div className="h-full flex mx-10">
+        <div className="h-full flex md:mx-10 mx-5">
           <div className="md:w-3/5 w-full flex flex-col justify-start gap-3 text-left text-white md:pl-28 md:pr-48 px-10 md:pt-40 pt-10">
               <h1 className="md:text-3xl text-xl"> Hello, My name is Spiro </h1>
               <h1 className="md:text-4xl text-2xl"> I am a <span className="text-violet-600"> Front-End Developer </span>  </h1>
@@ -21,17 +21,18 @@ export default function Home() {
             </div>
           </div>
 
-          <div className=" flex items-center justify-center w-2/5  ">
+          <div className=" flex items-center justify-center md:w-2/5  ">
           </div>
           
         </div>
       </main>
 
-      <section className=" min-h-screen">
-        <div>
-          <h1> Hello </h1>
+      <section className="bg-background-color min-h-screen">
+        <div className="mx-10 flex items-center ">
+          <h1 className="text-2xl text-white"> Hello </h1>
         </div>
       </section>
+      
     </>
   );
 }
